1b06b6ecb5747ac15bfd9a6121dcc0e4b45beb67
bulentselcuk <bulentselcuk@gmail.com>
2020-05-11 23:18:15 -0500

    cucumberversion4 working

---
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -0,0 +1,27 @@
+package com.vytrack.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        glue="com/vytrack/step_definitions",
+        features="src/test/resources/features",
+        dryRun=false,
+        strict=false,
+        tags = "@store_manager",
+//        tags="@sales_manager or @driver" // for two test
+
+//        tags = "@sales_manager",
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber1.json",
+                "rerun:target/rerun.txt"
+        }
+
+)
+public class CucumberRunner {
+
+
+}
